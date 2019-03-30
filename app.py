87127e25f8e713a4693e856864f48a453ca3f8be
rodatgithub <49103383+rodatgithub@users.noreply.github.com>
2019-03-30 21:32:11 +0100

    updating the colours
    tried a lighter theme with red ale instead of orange and branched it this time. and changed some vlaues for the IBU and ABV

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 ########### Set up the chart
-beers=['blood orange ale', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+beers=['blood red ale', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
 
 bitterness = go.Bar(
     x=beers,
-    y=[40, 60, 85, 75],
+    y=[55, 60, 85, 75],
     name='IBU',
     marker={'color':'lightred'}
 )
 alcohol = go.Bar(
     x=beers,
-    y=[7.0, 7.1, 9.2, 4.3],
+    y=[8.0, 7.1, 9.2, 4.3],
     name='ABV',
     marker={'color':'lightblue'}
 )
